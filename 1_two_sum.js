c355767299f8390674f3f85ffc9528ae2930bb69
amlodzianowska <an.mlodzianowska@gmail.com>
2022-01-19 17:57:28 +0100

    Day 1 #100DaysOfCode

---
+++ 1_two_sum.js
@@ -0,0 +1,19 @@
+
+var twoSum = function(nums, target) {
+    var sum;
+    for (let i=0; i<nums.length; i++) {
+        for (let j=0; j<nums.length; j++){
+            if (i != j) {
+                sum = nums[i] + nums[j]
+                if (sum == target){
+                    return [i,j]
+                }
+            }
+        }
+    }
+    return false
+};
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,5,6,10,18,27,35], 45))
+console.log(twoSum([], 9))
