dc5f482610d59ee3df7af8334febc27e915e25c4
amlodzianowska <an.mlodzianowska@gmail.com>
2022-01-20 11:14:23 +0100

    Day 2

--- 1_two_sum.js
+++ 1_two_sum.js
@@ -1,3 +1,5 @@
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
 var twoSum = function(nums, target) {
     var sum;
     for (let i=0; i<nums.length; i++) {
---
+++ 4_longest_common_prefix.js
@@ -0,0 +1,24 @@
+var longestCommonPrefix = function(strs) {
+    var minIdx = 0;
+    for (let i=0; i<strs.length; i++){
+        if (strs[i].length < strs[minIdx].length){
+            minIdx = i
+        }
+    }
+    const [...prefix] = strs[minIdx]
+    for (let i=0; i<strs.length; i++){
+        if (i==minIdx){
+            continue
+        }else{
+            for (let j=prefix.length-1; j>=0; j--){
+                if (strs[i][j] != prefix[j])
+                prefix.splice(j)
+            }
+        }
+    }
+    var longestPrefix = prefix.join("")
+    return longestPrefix
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
